94ac38d0ed8e6500512124b23cc1ce98c279565e
Jacob1225 <carlone.jacob@gmail.com>
2020-06-11 11:00:49 -0400

    merge conflict

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-<<<<<<< HEAD
 import  Leaderboard  from '../components/Leaderboard/index'
 import Pointsbutton from '../components/Leaderboard/PointsButton'
 import { action } from "@storybook/addon-actions";
 import Header from '../components/Leaderboard/Header'
 import LeaderboardList from '../components/Leaderboard/LeaderboardList';
-
-storiesOf('Leaderboard', module)
-	.add( 'Volunteer panel', () => <Leaderboard/>)
-	.add('Button', () => <Pointsbutton>Highest Points</Pointsbutton>)
-	.add('Header', () => <Header/>)
-	.add('Leaderboard List', () => <LeaderboardList/>)
-
-=======
-import { actions } from '@storybook/addon-actions';
 import RequestListItem from '../components/Request/RequestListItem';
 import AcceptButton from '../components/Request/AcceptButton';
 import RequestList from '../components/Request/RequestList';
@@ -22,9 +12,13 @@ import RequestForm from '../components/Request/RequestForm';
 import ReimbursementDropDown from '../components/Request/Reimbursement';
 import Groceries from '../components/Request/Groceries';
 
-<<<<<<< HEAD
-storiesOf('Card', module)
-	.add( 'Default Card', () => <h1>Hello World</h1>)
+storiesOf('Leaderboard', module)
+	.add( 'Volunteer panel', () => <Leaderboard/>)
+	.add('Button', () => <Pointsbutton>Highest Points</Pointsbutton>)
+	.add('Header', () => <Header/>)
+	.add('Leaderboard List', () => <LeaderboardList/>)
+
+
 
 
 storiesOf('Request', module)
@@ -35,8 +29,4 @@ storiesOf('Request', module)
 	.add("ReimbursementDropDown", () => <ReimbursementDropDown/>)
 	.add("Groceries", () => <Groceries/>)
 	
-=======
-storiesOf('Task', module)
-	.add( 'Default Card', () => <h1>Hello World</h1>)
->>>>>>> master
->>>>>>> master
+
