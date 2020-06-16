19ba216861c12e40f8154668eb00b21bbf1f232d
Ron Juarez <ronstewartjuarez@gmail.com>
2020-06-16 15:21:05 -0400

    added search component

--- src/components/Request/NewRequest.jsx
+++ src/components/Request/NewRequest.jsx
@@ -1,28 +1,14 @@
-import React, {useEffect}  from 'react';
+import React from 'react';
 // import ReimbursementDropDown from './Reimbursement';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import Groceries from './Groceries'
-import Header from './Header'
+import Groceries from './Groceries';
+import Header from './Header';
+import Search from './Search';
 // import { NavLink } from "react-router-dom";
 import { FormControl, FormHelperText, InputLabel, Button, Grid, Typography, Select, MenuItem } from '@material-ui/core';
 
 // import axios from 'axios';
-import { useLoadScript} from "@react-google-maps/api";
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-} from "@reach/combobox";
-import "@reach/combobox/styles.css";
-const libraries = ["places"];
-
 
 
 export default function NewRequest ({
@@ -37,75 +23,7 @@ export default function NewRequest ({
   setDeliveryAddress,
 }) {
 
-  const {isLoaded, loadError} = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
-     libraries 
-    })
-    
-
-  function Search() {
-    const {
-      ready,  
-      value, 
-      suggestions: {status, data}, 
-      setValue, 
-      clearSuggestions
-    } = usePlacesAutocomplete({
-    
-    })
-    
-    useEffect(() => {
-        console.log('value', value)
-    }, [value])
-
-    useEffect(() => {
-      console.log('new')
-  }, [])
-    return (
-      <div>
-      <Combobox 
-          
-          onSelect={async (address) => {
-            setValue(address, false);
-            
-            clearSuggestions();
-            try {
-              const results = await getGeocode({address})
-              setDeliveryAddress(results[0].formatted_address)
-              const coords = await getLatLng(results[0])
-              setCoords(coords.lat, coords.lng)
-        
-            } catch(error) {
-              console.log(error)
-            }
-          }}>
-
-          
-        <ComboboxInput
-          style={{width:"100%"}}
-          value={value} 
-          onChange={(e) => {setValue(e.target.value)}}
-          setValue={setValue}
-          disabled={!ready}
-          placeholder="Enter your address"
-        />
-        <ComboboxPopover>
-        <ComboboxList>
-          {status === "OK" && data.map(({id, description}) => 
-          (<ComboboxOption key={id} value ={description}/>
-          ))}
-          </ComboboxList>
-  
-        </ComboboxPopover>
-      </Combobox>
-      </div>
-      
-  
-     )
-  }
-   
-
-
+ 
 
   return (
     <Grid
@@ -134,7 +52,7 @@ export default function NewRequest ({
         
           <Typography>Delivery Address</Typography>
 
-            <div><Search/></div>
+            <div><Search coords={setCoords} address={setDeliveryAddress}/></div>
 
   
 
---
+++ src/components/Request/Search.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react';
+import { useLoadScript} from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+
+
+
+const libraries = ["places"];
+const {isLoaded, loadError} = useLoadScript({
+  googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
+   libraries 
+  })
+  
+
+  export default function Search({
+    setCoords,
+    setAddress
+  }) {
+  const {
+    ready,  
+    value, 
+    suggestions: {status, data}, 
+    setValue, 
+    clearSuggestions
+  } = usePlacesAutocomplete({
+  
+  })
+  
+  useEffect(() => {
+      console.log('value', value)
+  }, [value])
+
+  useEffect(() => {
+    console.log('new')
+}, [])
+
+
+  return (
+    <div>
+    <Combobox 
+        
+        onSelect={async (address) => {
+          setValue(address, false);
+          
+          clearSuggestions();
+          try {
+            const results = await getGeocode({address})
+            setAddress(results[0].formatted_address)
+            const coords = await getLatLng(results[0])
+            setCoords(coords.lat, coords.lng)
+      
+          } catch(error) {
+            console.log(error)
+          }
+        }}>
+
+        
+      <ComboboxInput
+        style={{width:"100%"}}
+        value={value} 
+        onChange={(e) => {setValue(e.target.value)}}
+        setValue={setValue}
+        disabled={!ready}
+        placeholder="Enter your address"
+      />
+      <ComboboxPopover>
+      <ComboboxList>
+        {status === "OK" && data.map(({id, description}) => 
+        (<ComboboxOption key={id} value ={description}/>
+        ))}
+        </ComboboxList>
+
+      </ComboboxPopover>
+    </Combobox>
+    </div>
+    
+
+   )
+}
+ 
+
