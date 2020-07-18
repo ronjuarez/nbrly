17f447dbaa19ecbc5375230d40350e865e7e5147
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-07-18 19:09:40 -0400

    Add an axios request to user database

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import FacebookLogin from 'react-facebook-login';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-
+import { useEffect } from 'react';
 
 export default function Login({
     handleChange,
@@ -15,6 +15,17 @@ export default function Login({
 
     let responseFacebook = response => console.log(response);
 
+    useEffect(() => {
+        axios.get('http://localhost:3000/users').then((response) => {
+            console.log(response.data.body);
+        });
+    }, [responseFacebook]);
+    // Loop through the users 
+    // If a user's email is in the database AND they're a fbUser, log them in 
+    // Otherwise, prompt them with a component that tells them they're registering 
+    // Then, add them to the database and log them in 
+    // Problems: this useEffect is being triggered several times - reduce the about of times it's being triggered 
+
     let componentClicked = () => console.log('clicked');
     let fbData;
 
@@ -23,7 +34,7 @@ export default function Login({
     } else {
         fbData = (
             <FacebookLogin
-                appId="270320127388791"
+                appId="285017142581524"
                 autoLoad={true}
                 fields="name,email,picture"
                 onClick={componentClicked}
