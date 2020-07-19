d03e6b83b2609c1584c535f9b847601fadc1fa3e
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-07-19 14:00:00 -0400

    Started making space for pop up registration component

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -21,19 +21,22 @@ export default function Login({
         axios.get('http://localhost:3000/users').then((response) => {
             for (let user of response.data.body) {
                 if ((user.email === facebookUser.email) && user.fb_user === true) {
+                    console.log("Facebook user found in database");
                     // set Facebook User to current user 
+                    // --> handleChange, changeUser 
                 } else {
                     // Prompt them with a registration component, then add them to the database and set them as current user 
-
+                    fbRegisterComponent = <h1>"Register PopUp"</h1>;
                 }
             };
-        });
+        }).catch((response) => console.log(response));
     }, facebookUser);
     // Problems: this useEffect is being triggered several times - reduce the about of times it's being triggered 
     // What are the arguments for useEffect? 
 
     let componentClicked = () => console.log('clicked');
     let fbData;
+    let fbRegisterComponent; 
 
     if (loggedInStatus === "Logged in") {
         fbData = null;
